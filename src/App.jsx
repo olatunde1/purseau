66408a131c0cc4563b0eab681ba70e080dccc3d9
dexie14 <adeluadeoniye14@gmail.com>
2025-03-08 11:53:20 +0100

    feat: added packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import './App.css'
-import Layout from './components/layout'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import Product from './pages/Product'
-import Cloth from './pages/Cloth'
-import Bags from './pages/Bags'
-import AboutUs from './pages/AboutUs'
+import "./App.css";
+import Layout from "./components/layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Product from "./pages/Product";
+import Cloth from "./pages/Cloth";
+import Bags from "./pages/Bags";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/Home' index element={<Home />} /> {/* Default route */}
-          <Route path='Shop' element={<Shop />} />
-          {/* <Route path='/Product' element={<Product />} /> */}
-          <Route path='/Cloth' element={<Cloth />} />
-          <Route path='/Bags' element={<Bags />} />
-          <Route path='/AboutUs' element={<AboutUs />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/Home" index element={<Home />} /> {/* Default route */}
+        <Route path="Shop" element={<Shop />} />
+        {/* <Route path='/Product' element={<Product />} /> */}
+        <Route path="/Cloth" element={<Cloth />} />
+        <Route path="/Bags" element={<Bags />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
