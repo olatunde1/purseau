039cd04872440d6794e3d7757dde2c6ee890bc29
Adetunji <idowuolatunde32@gmail.com>
2025-03-10 13:57:57 +0100

    fix: pages fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
 import BlogDetails from "./pages/BlogDetails";
 import SignUp from "./components/Auth/SignUp";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 
 function App() {
   return (
