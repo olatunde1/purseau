6fc534e1ce238545a3e331bdcaecf6190ad7af31
Adetunji <idowuolatunde32@gmail.com>
2025-03-10 14:03:13 +0100

    fix: pages fixed

--- src/pages/Bags.jsx
+++ src/pages/Bags.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Footer } from '@/components/Footer'
+import { Footer } from '@/components/footer'
 import Explore from '../components/ExploreCategories'
 import RecentlyViewed from '@/components/RecentlyViewed'
 import ProductCloth from '../components/ProductCloth'
