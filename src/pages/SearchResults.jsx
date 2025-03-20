d8357638153196b9269e94486f8bdc45a621410c
Adetunji <idowuolatunde32@gmail.com>
2025-03-20 14:43:35 +0100

    fix

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -4,7 +4,7 @@ import { useProductStore } from "@/store/productStore";
 import useGetProducts from "@/hooks/api/queries/useGetProducts";
 
 // Components
-import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/footer";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SubMenu from "../components/SubMenu";
-import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/footer";
 import Explore from "../components/ExploreCategories";
 import Pagination from "@/components/Pagination";
 import RecentlyViewed from "@/components/RecentlyViewed";
