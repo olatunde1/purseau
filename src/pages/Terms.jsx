d35ca87be254cc810781a9e92dbb74b15b542775
Adetunji <idowuolatunde32@gmail.com>
2025-03-29 01:32:43 +0100

    fix: comment out Footer component in Terms and Conditions page

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -6,7 +6,7 @@ import {
     AccordionTrigger,
   } from "@/components/ui/accordion"
 import { StayLoop } from "@/components/StayLoop"
-import { Footer } from "@/components/Footer"
+// import { Footer } from "@/components/Footer"
 
   
   export default function TermsAndConditions() {
@@ -316,7 +316,7 @@ import { Footer } from "@/components/Footer"
       </Accordion>
       </div>
     <StayLoop />
-    <Footer />
+    {/* <Footer /> */}
     </>
     
   };
